ebeadd641fdf4d55f21d31a7c4052378ac694674
meminaydogan <meminaydogan99@gmail.com*git config --global user.email>
2024-03-20 15:24:57 +0300

    3.gün sonu

---
+++ src/test/java/tests/day03_maven/C01_MavenilkTest.java
@@ -0,0 +1,22 @@
+package tests.day03_maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+
+public class C01_MavenilkTest {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.testotomasyonu.com");
+
+        ReusableMethods.bekle(3);
+
+    }
+
+}
---
+++ src/test/java/tests/day03_maven/C02_ExternalWebDriverKullanma.java
@@ -0,0 +1,23 @@
+package tests.day03_maven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+
+public class C02_ExternalWebDriverKullanma {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.testotomasyonu.com");
+
+        ReusableMethods.bekle(3);
+        driver.quit();
+    }
+}
